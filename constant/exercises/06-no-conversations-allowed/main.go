cbb096c63ddba348deaa121e2b67ab3c3c1b93d0
wisnu <aryswadanawisnuu@gmail.com>
2023-12-02 01:30:07 +0700

    wisnu

--- constant/exercises/06-no-conversations-allowed/main.go
+++ constant/exercises/06-no-conversations-allowed/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+    // 1. Declare the number of rows and columns
+    const rows, cols = 3, 3
+
+    // 2. Create a 2D slice (matrix)
+    matrix := make([][]int, rows)
+    for i := range matrix {
+        matrix[i] = make([]int, cols)
+    }
+
+    // 3. Fill the matrix with numbers
+    num := 1
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            matrix[i][j] = num
+            num++
+        }
+    }
+
+    // 4. Print the matrix
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            fmt.Printf("%d ", matrix[i][j])
+        }
+        fmt.Println()
+    }
 }
